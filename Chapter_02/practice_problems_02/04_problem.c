66a056126496e1859d11edc32593136f9f749064
Niladri Pal <niladripal0810@gmail.com>
2026-08-06 22:33:01 +0530

    update Chapter_02/practice_problems_02/04_problem.c

---
+++ Chapter_02/practice_problems_02/04_problem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+     int a, b;
+     printf("enter the value of a: ");
+     scanf("%d", &a);
+     printf("enter the value of b: ");
+     scanf("%d", &b);
+     
+     printf("So a=%d, b=%d\n", a, b);
+     
+     // Swapping logic
+     a = a + b;
+     b = a - b;
+     a = a - b;
+     
+     printf("Swapped a=%d, b=%d\n", a, b);
+    
+     return 0;
+
+}
