47e4785d21d4925485a4e00b8102db9a86f58de2
RipperdocNiladri <niladripal0810@gmail.com>
2026-08-08 23:42:41 +0530

    refactor: standardize main function formatting and improve code readability in problem files

--- Chapter_02/practice_problems_02/01_problem.c
+++ Chapter_02/practice_problems_02/01_problem.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     printf("Wheather your number is divisible by 7 or not\n");
     int a;
     printf("Enter your number: ");
     scanf("%d", &a);
-    printf("The remainder when %d is divided by 7 is %d\n", a, a%7);
+    printf("The remainder when %d is divided by 7 is %d\n", a, a % 7);
     // If the remainder is 0, then the number is divisible by 7
     return 0;
 }
